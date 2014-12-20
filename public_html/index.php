5208fe310ca8db41c8b024f7fa83d83546cc9db0
Javad <J.Evazzadeh@gmail.com>
2014-12-20 20:18:46 +0330

    2

--- public_html/index.php
+++ public_html/index.php
@@ -3,47 +3,45 @@
 if (!defined('LC_MESSAGES')) 
 	{
 		define('LC_MESSAGES', 6);
-		var_dump("not defined");
+		var_dump("LC_MESSAGES not defined");
 	}
+
+// $lang = "en"; $locale = "en_GB";
+// if (isset($_GET["lang"]) && $_GET["lang"] == "fa"){ $lang = "fa"; $locale = "fa_IR"; }
+// if (isset($_GET["lang"]) && $_GET["lang"] == "de"){ $lang = "de"; $locale = "de_DE"; }
 
 
 $lang = "en"; $locale = "en_US";
-if ($_GET["lang"] == "de"){ $lang = "de"; $locale = "de_DE"; }
-if ($_GET["lang"] == "fa"){ $lang = "fa"; $locale = "fa_IR"; }
-
-var_dump($locale);
-
-$locality = 'en_US'; // locality should be determined here
-if (defined('LC_MESSAGES')) {
-    setlocale(LC_MESSAGES, $locality); // Linux
-} else {
-    putenv("LC_ALL={$locality}"); // windows
-    var_dump("windows");
+if(isset($_GET["lang"]))
+{
+	$locale =$_GET["lang"];
 }
 
+// var_dump($locale);
+// var_dump( getenv('LANG'), putenv('LANG=en_GB'), getenv('LANG') );
 
-// language="fr_FR";
-// putenv("LC_ALL=$language");
-// setlocale(LC_ALL, $language);
-// bindtextdomain("messages", "./locale");
-// textdomain("messages");
+
+putenv("LC_ALL=$locale"); // wrong in windows
+putenv('LC_ALL='.$locale);
+
+setlocale(LC_ALL, $locale);
+setlocale(LC_MESSAGES, $locale);
+bindtextdomain('messages', __DIR__ . '/locale');
+textdomain("messages");
+
+
 // print "”._(“hi! This website is written in English.”).”\n”;
-// bindtextdomain('gettext', __DIR__ . '/locale');
 
-
-// // putenv("LC_ALL=$locale");
-// // putenv("LANG=$lang");  <- WRONG!
-// putenv('LC_ALL='.$lang);
 
 // putenv("LC_ALL=en_US");
-// setlocale(LC_ALL, $locale);
 
-// setlocale(LC_MESSAGES, $locale);
+
+
 // bindtextdomain("messages", "./locale");
 // textdomain("messages");
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?=$lang?>" lang="<?=$lang?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?=$locale?>" lang="<?=$locale?>">
 <head>
 	<title><?=_("Hello World!")?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
