4ec96778b5698393318401350eb8a19ccab52f2e
Javad <J.Evazzadeh@gmail.com>
2014-12-25 22:42:52 +0330

    check lang

--- includes/cls/autoload.php
+++ includes/cls/autoload.php
@@ -10,35 +10,36 @@ class autoload{
 
 
 
-		$langs = array(
-			'en_US',
-			'fa_IR',
-			'ar_SA'
-			);
-		if(isset($_GET['lang']) && array_search($_GET['lang'], $langs) !==false){
-			setcookie("lang", $_GET['lang'], time()+31536000, '/');
-			$locale = $langs[array_search($_GET['lang'], $langs)];
-		}elseif(isset($_COOKIE['lang']) && array_search($_COOKIE['lang'], $langs) !==false){
-			$locale = $langs[array_search($_COOKIE['lang'], $langs)];
-		}else{
-			$locale = "en_US";
-		}
-		var_dump($locale); 
-		setlocale(LC_MESSAGES, $locale);
-		putenv('LC_MESSAGES=$locale');
-		putenv("LC_ALL=$locale");
-		setlocale(LC_ALL, $locale);
-		bindtextdomain("index", dir_includes."languages/");
-		bind_textdomain_codeset("index", "UTF-8");
-		textdomain("index");
-		var_dump(_('Version'));
+		// $langs = array(
+		// 	'en_US',
+		// 	'fa_IR',
+		// 	'ar_SA'
+		// 	);
+		// if(isset($_GET['lang']) && array_search($_GET['lang'], $langs) !==false){
+		// 	setcookie("lang", $_GET['lang'], time()+31536000, '/');
+		// 	$locale = $langs[array_search($_GET['lang'], $langs)];
+		// }elseif(isset($_COOKIE['lang']) && array_search($_COOKIE['lang'], $langs) !==false){
+		// 	$locale = $langs[array_search($_COOKIE['lang'], $langs)];
+		// }else{
+		// 	$locale = "en_US";
+		// }
+		// var_dump($locale); 
+		// setlocale(LC_MESSAGES, $locale);
+		// putenv('LC_MESSAGES=$locale');
+		// putenv("LC_ALL=$locale");
+		// setlocale(LC_ALL, $locale);
+		// bindtextdomain("index", dir_includes."languages/");
+		// bind_textdomain_codeset("index", "UTF-8");
+		// textdomain("index");
+		// var_dump(_('Version'));
 
-if (!locale_emulation()) {
-	print "<p>locale '$locale' is supported by your system, using native gettext implementation.</p>\n";
-}
-else {
-	print "<p>locale '$locale' is <strong>not</strong> supported on your system, using custom gettext implementation.</p>\n";
-}
+		if (!locale_emulation()) {
+			var_dump("locale is supported by your system, using native gettext implementation.");
+		}
+		else {
+			var_dump("locale is <strong>not</strong> supported on your system, using custom gettext implementation.");
+		}
+
 	}
 }
 ?>
