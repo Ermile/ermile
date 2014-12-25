b2da132c3115fc82ee9187baf2b1750d4a3fb599
Javad <J.Evazzadeh@gmail.com>
2014-12-25 22:19:18 +0330

    check for work on server

--- includes/cls/autoload.php
+++ includes/cls/autoload.php
@@ -10,33 +10,6 @@ class autoload{
 
 
 
-		// Define Project Attributes **************************************************************************************
-		/**
-		 * in coming soon period show public_html/pages/coming/ folder
-		 * developer must set get parameter like site.com/dev=anyvalue
-		 * for disable this attribute turn off it from config.php in project root
-		 */
-		define("DOMAIN"		, $_SERVER["SERVER_NAME"]);
-
-		if(CommingSoon && isset($_GET['dev'])){
-			setcookie('preview','yes',time() + 30*24*60*60,'/','.'.$_SERVER["SERVER_NAME"]);
-		}
-		elseif(CommingSoon && !isset($_COOKIE["preview"])){
-			header('Location: http://'.$_SERVER["SERVER_NAME"]."/static/page/coming/", true, 302);
-			exit();
-		}
-
-		/**
-		 * Localized Language, defaults to English.
-		 *
-		 * Change this to localize Saloos. A corresponding MO file for the chosen
-		 * language must be installed to content/languages. For example, install
-		 * fa_IR.mo to content/languages and set LANGUAGE to 'fa_IR' to enable Persian
-		 * language support.
-		 */
-
-
-
 		$langs = array(
 			'en_US',
 			'fa_IR',
@@ -59,65 +32,13 @@ class autoload{
 		bind_textdomain_codeset("index", "UTF-8");
 		textdomain("index");
 		var_dump(_('Version'));
-		var_dump("لطفا اون چیزی که فکر میکنی همه ازش استفاده می کنند رو توی هسته نیار.\nشاید درست نباشه.");
-		var_dump("فکر نکن که هرچی که شما نیاز داری بقیه هم نیاز دارن");
-		var_dump("هسته از اسمش مشخصه، یعنی هسته نه یک سیستم مدیریت محتوا");
-		var_dump("یه نگاهی به کد‌اگنایتر، سمفونی، زند و یا هر کوفت و زهرمار دیگه بنداز بعد بیا توی هسته دست بزن");
-		var_dump("می تونی یه پروژه از سالوس تعریف کنی و بعد هرکاری خواستی توش انجام بدی.");
 
-
-
-		if(MultiLanguage)
-		{
-			if (isset($_GET["lang"]))
-				$language = $_GET["lang"];
-
-			elseif(isset($_COOKIE["lang"]))
-				$language = $_COOKIE["lang"];
-
-			elseif(substr(DOMAIN, strrpos(DOMAIN, '.') + 1)=='ir')
-				$language = "fa_IR";
-			else
-				$language = "en_US";
-
-
-			// save language preference for future page requests
-			setcookie('lang',$language,time() + 30*24*60*60,'/','.'.DOMAIN);
-
-			// use saloos php gettext function
-			require_once(ilib.'gettext/gettext.inc');
-
-			// gettext setup
-			T_setlocale(LC_MESSAGES, $language);
-			// Set the text domain as 'messages'
-			T_bindtextdomain('messages', root.'includes/languages');
-			T_bind_textdomain_codeset('messages', 'UTF-8');
-			T_textdomain('messages');
-		}
-
-		/**
-		 * If DEBUG is TRUE you can see the full error description, If set to FALSE show userfriendly messages
-		 * change it from project config.php
-		 */
-		if (DEBUG)
-		{
-			error_reporting(E_ALL);
-			ini_set('display_errors'		,'On');
-			ini_set('display_startup_errors','On');
-			ini_set('error_reporting'		,'E_ALL | E_STRICT');
-			ini_set('track_errors'			,'On');
-			ini_set('display_errors'		,1);
-			error_reporting(E_ALL);
-		}
-
-		/**
-		 * A session is a way to store information (in variables) to be used across multiple pages.
-		 * Unlike a cookie, the information is not stored on the users computer.
-		 * access to session with this code: $_SESSION["test"]
-		 */
-		session_name(DOMAIN);
-		session_set_cookie_params(0, '/', '.'.DOMAIN);
-		session_start();
+if (!locale_emulation()) {
+	print "<p>locale '$locale' is supported by your system, using native gettext implementation.</p>\n";
+}
+else {
+	print "<p>locale '$locale' is <strong>not</strong> supported on your system, using custom gettext implementation.</p>\n";
+}
 	}
 }
 ?>
