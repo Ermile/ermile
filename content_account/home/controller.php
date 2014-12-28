9e262cc0b85cf08585bdf5a7f1f5439f0c7a4a3b
Javad <J.Evazzadeh@gmail.com>
2014-12-28 13:19:57 +0330

    fix account display desc and clean folders

--- content_account/home/controller.php
+++ content_account/home/controller.php
@@ -8,11 +8,14 @@ class controller extends \mvc\controller
 		$this->route("/^login|signup|recovery|logout$/", function(){
 			$module	= \lib\router::get_real_url();
 			$this->model_name	= 'content\\'.$module.'\model';
+			$this->display_name	= 'content_account\\'.$module.'\display.html';
+			// $this->display_name	= 'content_account\home\display.html';
 			$this->post($module)->ALL($module);
 		});
 		$this->route("/^verification|changepass$/", function(){
 			$module	= \lib\router::get_real_url();
 			$this->model_name	= 'content\\'.$module.'\model';
+			$this->display_name	= 'content_account\\'.$module.'\display.html';
 			$this->put($module)->ALL($module);
 		});
 	}
--- content_account/login/model.php
+++ content_account/login/model.php
@@ -2,6 +2,12 @@
 namespace content\login;
 
 class model extends \mvc\model{
+	public function post_login()
+	{
+		\lib\debug::fatal("login", 'username', 'form');
+		// var_dump("signup");
+		// exit();
 
+	}
 }
 ?>
