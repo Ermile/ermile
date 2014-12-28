3c556c5c5a901fc4cb24186f03b9c8055d7a8dc8
baravak <itb.baravak@gmail.com>
2014-12-28 21:18:44 +0330

    f

--- content_account/home/controller.php
+++ content_account/home/controller.php
@@ -5,7 +5,12 @@ class controller extends \mvc\controller
 {
 	function config()
 	{
-		$this->route("/^login|signup|recovery|logout$/", function(){
+		$this->route("/^$/", function(){
+			$this->redirector()->set_url("login");
+			$this->redirector()->redirect();
+		});
+		$this->route("/^(login|signup|recovery|logout)$/", function(){
+
 			$module	= \lib\router::get_real_url();
 			$this->model_name	= 'content\\'.$module.'\model';
 			$this->display_name	= 'content_account\\'.$module.'\display.html';
