ab92ed22f380995cb62fe62e6b53ade91534a967
baravak <itb.baravak@gmail.com>
2014-12-29 19:41:28 +0330

    f

--- content_account/home/controller.php
+++ content_account/home/controller.php
@@ -3,14 +3,14 @@ namespace content\home;
 
 class controller extends \mvc\controller
 {
-	function config()
+	function _route()
 	{
 		$this->route("/^$/", function(){
 			$this->redirector()->set_url("login")->redirect();
 		});
 
 		$this->route("/^(login|signup|recovery|smsdelivery)$/", function(){
-
+			var_dump(60);
 			$module	= \lib\router::get_real_url();
 			$this->model_name	= 'content\\'.$module.'\model';
 			$this->display_name	= 'content_account\\'.$module.'\display.html';
