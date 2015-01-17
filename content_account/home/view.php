f5e6de8105ed6aa286cf55c8deb9e7a7db2995bc
Javad <J.Evazzadeh@live.com>
2015-01-17 18:42:49 +0330

    some change for fix title and push state

--- content_account/home/view.php
+++ content_account/home/view.php
@@ -5,57 +5,62 @@ class view extends \mvc\view
 {
 	public function config()
 	{
-		// $this->include->datatable		= false;
-		// $this->include->jquery			= true;
-		// $this->include->fontawesome		= false;
-		$this->include->telinput		= true;
-		$this->include->customcss		= false;
-		$this->include->customjs		= false;
-		$this->data->bodyclass			= 'unselectable';
+		// $this->include->datatable   = false;
+		// $this->include->jquery      = true;
+		// $this->include->fontawesome = false;
+		$this->include->telinput       = true;
+		$this->include->customcss      = false;
+		$this->include->customjs       = false;
+		$this->data->bodyclass         = 'unselectable';
 		
-		$this->data->myform				= 'account';
-		$this->data->site_slogan		= T_("One Account for all services");
+		$this->data->myform            = 'account';
+		$this->data->site_slogan       = T_("One Account for all services");		
 
-		if($this->data->module =='login'){
-			$this->data->page_desc	= T_('Login');
-		}
-		elseif($this->data->module =='signup'){
-			$this->data->page_desc	= T_('Create an account');
-		}
-		elseif($this->data->module =='verification'){
-			$this->data->page_desc	= T_('Verificate');
-			$this->data->mobile			= \lib\utility::get('mobile');
-		}
-		elseif($this->data->module =='verificationsms'){
-			$this->data->page_desc	= T_('Verificate');
-		}
-		elseif($this->data->module =='recovery'){
-			$this->data->page_desc	= T_('Recovery');
-		}
-		elseif($this->data->module =='changepass'){
-			$this->data->page_desc	= T_('Change password');
-			$this->include->telinput	= false;
-		}
-		elseif($this->data->module =='smsdelivery'){
-			$this->data->page_desc	= T_('SMS Delivery');
-			$this->include->telinput	= false;
-		}
-		elseif($this->data->module =='smscallback'){
-			$this->data->page_desc	= T_('SMS Callback');
-			$this->include->telinput	= false;
-		}
-		else
+		switch ($this->data->module)
 		{
-			$this->data->page_desc	= T_('Ermile');
-			return;
+			case 'login':
+				$this->data->page_desc	= T_('Login');
+				break;
+
+			case 'signup':
+				$this->data->page_desc	= T_('Create an account');
+				break;
+
+			case 'verification':
+				$this->data->page_desc	= T_('Verificate');
+				$this->data->mobile			= \lib\utility::get('mobile');
+				break;
+
+			case 'verificationsms':
+				$this->data->page_desc	= T_('Verificate');
+				break;
+
+			case 'recovery':
+				$this->data->page_desc	= T_('Recovery');
+				break;
+
+			case 'changepass':
+				$this->data->page_desc	= T_('Change password');
+				$this->include->telinput	= false;
+				break;
+
+			case 'smsdelivery':
+				$this->data->page_desc	= T_('SMS Delivery');
+				$this->include->telinput	= false;
+				break;
+
+			case 'smscallback':
+				$this->data->page_desc	= T_('SMS Callback');
+				$this->include->telinput	= false;
+				break;
+
+			default:
+				$this->data->page_desc	= T_('Ermile');
+				break;
 		}
 
-
-		$this->data->page_title		= $this->data->page_desc;
+		$this->global->title		= $this->data->page_desc.' | '.$this->data->site_title;
 		$form = $this->createform('.'.$this->data->myform, $this->data->module);
-
-		// $form = $this->form("@jibres.accounts");
-		// $form->before('account_slug', 'account_title');
 	}
 }
 ?>
