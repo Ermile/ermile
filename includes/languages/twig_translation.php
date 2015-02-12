bf6eab021611cb2a97b0b3202d23ecdb60af323c
Javad <J.Evazzadeh@live.com>
2015-02-12 06:15:36 +0330

    update db files and translation

--- includes/languages/twig_translation.php
+++ includes/languages/twig_translation.php
@@ -29,6 +29,10 @@ function transtext()
 	echo T_("click here to login to your account");                                   // Line 6
 	echo T_("are you remember your password!?");                                      // Line 6
 
+	//------------------------------------------------content_account/main/layout.html
+	echo T_("Ermile is our brand");                                                   // Line 67
+	echo T_("Experience our service in your language");                               // Line 72
+
 	//---------------------------------------content_account/verification/display.html
 	echo T_("we send a verification code for you");                                   // Line 6
 	echo T_("please check your mobile and enter the code");                           // Line 7
@@ -43,26 +47,26 @@ function transtext()
 	echo T_("Talambar");                                                              // Line 37
 	echo T_("is ready to use at alfa version.");                                      // Line 37
 	echo T_("our new service is comming soon.");                                      // Line 38 Seperate
-	echo T_("Ermile is our brand");                                                   // Line 48
-	echo T_("Experience our service in your language");                               // Line 53
 
 	//----------------------------------------------------content_cp/home/display.html
 	echo T_("Logout");                                                                // Line 46
-	echo T_("Home");                                                                  // Line 38
+	echo T_("Home");                                                                  // Line 40
 
 	//-------------------------------------------------content_cp/main/xhr-layout.html
-	echo T_("Options");                                                               // Line 23 Seperate
-	echo T_("Go to");                                                                 // Line 71
-	echo T_("Add New Record");                                                        // Line 37 Seperate
-	echo T_("Actions");                                                               // Line 50
-	echo T_("Edit");                                                                  // Line 69
-	echo T_("Delete");                                                                // Line 92
-	echo T_("delete record");                                                         // Line 87 Seperate
-	echo T_("seriously, are you sure? There's no coming back.");                      // Line 88 Seperate
-	echo T_("Cancel");                                                                // Line 90
-	echo T_("or");                                                                    // Line 91
+	echo T_("Options");                                                               // Line 25 Seperate
+	echo T_("Go to");                                                                 // Line 34 Seperate
+	echo T_("Add New Record");                                                        // Line 39 Seperate
+	echo T_("Actions");                                                               // Line 52
+	echo T_("Edit");                                                                  // Line 71
+	echo T_("Delete");                                                                // Line 94
+	echo T_("delete record");                                                         // Line 89 Seperate
+	echo T_("seriously, are you sure? There's no coming back.");                      // Line 90 Seperate
+	echo T_("Cancel");                                                                // Line 92
+	echo T_("or");                                                                    // Line 93
 
 	//-----------------------------------------------------content_cp/main/layout.html
+	echo T_("Edit this record");                                                      // Line 70
+	echo T_("Delete this record");                                                    // Line 73
 
 	//---------------------------------------------------includes/cls/macro/forms.html
 	echo T_("Please select one item");                                                // Line 49 Seperate
