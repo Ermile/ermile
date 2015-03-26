a4a7e8b93fd9f91995ec669c8f876ad90987e602
Mahdi Dibaiee <mdibaiee@aol.com>
2015-03-27 02:50:38 +0430

    [contextmenu] hotkey and extend

--- includes/cls/mahdi/js/src/tools/contextmenu.js
+++ includes/cls/mahdi/js/src/tools/contextmenu.js
@@ -5,24 +5,27 @@
 
   var id = 1;
 
+  var list = [];
+
   var ContextMenu = function ContextMenu(options) {
     _.extend(this, _.omit(defaults, 'item'), _.omit(options, 'item'));
     this.init(options);
+    list.push(this);
   };
 
   ContextMenu.prototype = {
+    extend: function(element) {
+      var ops = {items: this.items, jquery: $(element)};
+      return new ContextMenu(ops);
+    },
     init: function(options) {
+      var _super = this;
+      var $wrapper = this.$wrapper = this.createWrapper();
+      var $elements = this.$elements = this.jquery;
+
       this.$wrapper.trigger('contextmenu:init');
 
       options = options || {};
-      if(this.$wrapper) {
-        this.$wrapper.remove();
-
-        this.$elements.off('contextmenu');
-      }
-      var $elements = this.$elements = this.jquery;
-      var $wrapper = this.$wrapper = this.createWrapper();
-      var _super = this;
 
       this.items = this.items.map(function(item) {
         return _.defaults(item, defaults.item, options.item);
@@ -32,68 +35,56 @@
         e.preventDefault();
         e.stopPropagation();
 
-        $wrapper
-          .css({
-            position: 'absolute',
-            left: e.pageX + 5,
-            top: e.pageY + 5,
-            display: 'block',
-            visibility: 'hidden'
-          });
-
-        var offset = $wrapper.offset(),
-            width = $wrapper.width(),
-            height = $wrapper.height();
-        if(offset.left + width > $(window).width()) {
-          $wrapper.css({
-            left: e.pageX - width - 5
-          });
-        }
-        if(offset.top + height > $(window).height()) {
-          $wrapper.css({
-            top: e.pageY - height - 5
-          });
-        }
-
-        $wrapper.css('visibility', 'visible').show();
-        $wrapper.trigger('contextmenu:shown');
+        list.forEach(function(cm) {
+          cm.close();
+        });
+        _super.open(e);
       });
 
-      $wrapper.on('close', function() {
-        $wrapper.hide();
-        $wrapper.trigger('contextmenu:close');
-      });
+      $wrapper.on('close', this.close.bind(this));
 
       $(window).on('keydown', function(e) {
         var $li = $wrapper.find('li');
-        var active = $li.index('.active');
+        var active = $li.filter('.active').index();
 
         if (e.which === 38) {
           $li.removeClass('active')
              .eq(active-1)
              .addClass('active');
 
-          $wrapper.trigger('contextmenu:selection:change');
+          $wrapper.trigger('contextmenu:selection:change', active-1)
         } else if (e.which === 40) {
+          if (active === $li.length-1) active = -1;
           $li.removeClass('active')
              .eq(active+1)
              .addClass('active');
 
-          $wrapper.trigger('contextmenu:selection:change');
+          $wrapper.trigger('contextmenu:selection:change', active+1)
         } else if (e.which === 13) {
-          var $target = 
-               $li.removeClass('active')
-               .eq(active)
-               .get(0);
-          $target.click();
+          var $target =
+                $li.removeClass('active')
+                .eq(active)
+                .addClass('active')
+                .get(0);
 
-          _.each($target.children, function(child) {
-            child.click();
+          $target.children[0].click();
+          $wrapper.trigger('contextmenu:selection:trigger', active)
+                  .trigger('contextmenu:selection:trigger:'+active);
+        } else {
+          _super.items.forEach(function(item, i) {
+            if (item.hotkey && e.which === item.hotkey.toUpperCase().charCodeAt(0)) {
+              var $target =
+                    $li.removeClass('active')
+                    .eq(i)
+                    .addClass('active')
+                    .get(0);
+
+              $target.children[0].click();
+              $wrapper.trigger('contextmenu:selection:trigger', i)
+                      .trigger('contextmenu:selection:trigger:'+i);
+            }
           });
-
-          $wrapper.trigger('contextmenu:selection:trigger');
         }
-
       });
 
       $(document).ready(function() {
@@ -121,11 +112,58 @@
       this.$wrapper.trigger('contextmenu:update');
       return this.items.length;
     },
+    setItems: function(items) {
+      this.items = items;
+      this.createItems();
+
+      this.$wrapper.trigger('contextmenu:update');
+      return items.length;
+    },
+    open: function(e) {
+      var $wrapper = this.$wrapper;
+      $wrapper
+        .css({
+          position: 'absolute',
+          left: e.pageX + 5,
+          top: e.pageY + 5,
+          display: 'block',
+          visibility: 'hidden'
+        });
+
+      var offset = $wrapper.offset(),
+          width = $wrapper.width(),
+          height = $wrapper.height();
+      if(offset.left + width > $(window).width()) {
+        $wrapper.css({
+          left: e.pageX - width - 5
+        });
+      }
+      if(offset.top + height > $(window).height()) {
+        $wrapper.css({
+          top: e.pageY - height - 5
+        });
+      }
+
+      $wrapper.css('visibility', 'visible').show();
+      $wrapper.trigger('contextmenu:open');
+    },
+    close: function() {
+      this.$wrapper.find('li.active').removeClass('active');
+      this.$wrapper.css('visibility', 'hidden').hide();
+      this.$wrapper.trigger('contextmenu:close');
+    },
     createItems: function() {
       var $stack = $('<ul></ul>');
       for(var i = 0, len = this.items.length; i < len; i++) {
-        var item = _.extend(defaults.item, this.items[i]);
+        var item = _.extend({}, defaults.item, this.items[i]);
         var $el = $('<li data-id="' + i + '"><a ' + (item.link ? 'href="' + item.link + '"' : '') + '>' + item.text + '</a></li>');
+        if (item.hotkey) {
+          var $a = $el.find('a');
+          var html = $a.html();
+          var regex = new RegExp(item.hotkey, 'i');
+          var underlined = '<span style="text-decoration: underline">$&</span>';
+          $a.html(html.replace(regex, underlined));
+        }
         $stack = $stack.append($el);
       }
 
