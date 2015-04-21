242dc14e077717402cb55342f986c0c08af49bb9
Javad <J.Evazzadeh@gmail.com>
2015-04-21 21:47:11 +0430

    today changes on control panel!

--- content_cp/home/controller.php
+++ content_cp/home/controller.php
@@ -28,52 +28,44 @@ class controller extends \mvc\controller
 		}
 		else
 		{
-			$this->display_name	= 'content_cp\main\layout.html';
+			// on module root without child like /post
+			if($mychild)
+			{
+				$this->display_name	= 'content_cp\home\child.html';
+				//all("edit=.*")
+				if($mychild == 'delete')
+				{
+					// $this->model()->delete();
+					// $controller->route_check_true = true;
+					// $this->redirector()->set_url($mymodule); //->redirect();
+					$this->get($mychild)->ALL();		// @hasan: regular?
+					$this->post($mychild)->ALL();
+					// return;
+					// $this->redirector()->set_domain('cp.ermile.dev')->set_url('banks');
+				}
+				if($mychild == 'edit')
+				{
+					// var_dump($this->model()->datarow());
+					$this->get(null, 'child')->ALL();
+					$this->post($mychild)->ALL();
+				}
+				elseif($mychild == 'add')
+				{
+					$this->get(null, 'child')->ALL();
+					$this->post($mychild)->ALL();
+				}
+				elseif($mychild == 'options')
+				{
+					$this->post($mychild)->ALL();
+				}
+
+			}
+			else
+			{
+				$this->display_name	= 'content_cp\home\module.html';
+				$this->get(null, 'datatable')->ALL();
+			}
 		}
-
-		if($mymodule=='home')
-			return;
-
-		// var_dump($this->childparam($mychild));
-
-
-		// on module root without child like /post
-		if($mychild)
-		{
-			//all("edit=.*")
-			if($mychild == 'delete')
-			{
-				// $this->model()->delete();
-				// $controller->route_check_true = true;
-				// $this->redirector()->set_url($mymodule); //->redirect();
-				$this->get($mychild)->ALL();		// @hasan: regular?
-				$this->post($mychild)->ALL();
-				// return;
-				// $this->redirector()->set_domain('cp.ermile.dev')->set_url('banks');
-			}
-			if($mychild == 'edit')
-			{
-				// var_dump($this->model()->datarow());
-				$this->get(null, 'child')->ALL();
-				$this->post($mychild)->ALL();
-			}
-			elseif($mychild == 'add')
-			{
-				$this->get(null, 'child')->ALL();
-				$this->post($mychild)->ALL();
-			}
-			elseif($mychild == 'options')
-			{
-				$this->post($mychild)->ALL();
-			}
-
-		}
-		else
-		{
-			$this->get(null, 'datatable')->ALL();
-		}
-		// $this->model_name	= 'content_cp\\'.$mymodule.'\model';
-		// $this->post($mymodule)->ALL($mymodule);
 	}
 
 
