96a79fdb4be7df5d1c916633fbfaace4b3402452
Javad <J.Evazzadeh@gmail.com>
2015-05-29 08:36:44 +0430

    add server monitor to project
    -https://github.com/phpservermon/phpservermon

---
+++ public_html/monitor/config.php
@@ -0,0 +1,6 @@
+<?php
+define('PSM_DB_HOST', 'localhost');
+define('PSM_DB_NAME', 'monitor');
+define('PSM_DB_USER', 'ermile');
+define('PSM_DB_PASS', 'ermile@#$567');
+define('PSM_DB_PREFIX', 'psm_');
