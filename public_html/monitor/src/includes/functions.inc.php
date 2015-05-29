ed0ab14d32f46b5855f75238ba2a8d3c5a35797d
Javad <J.Evazzadeh@gmail.com>
2015-05-29 23:16:36 +0430

    change encoding

--- public_html/monitor/src/includes/functions.inc.php
+++ public_html/monitor/src/includes/functions.inc.php
@@ -387,7 +387,7 @@ function psm_update_available() {
  */
 function psm_build_mail($from_name = null, $from_email = null) {
 	$phpmailer = new \PHPMailer();
-	$phpmailer->Encoding = "base64";
+	$phpmailer->Encoding = "utf8";
 	$phpmailer->SMTPDebug = false;
 
 	if(psm_get_conf('email_smtp') == '1') {
