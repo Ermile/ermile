ab99b0b778f49bb288d5bdde25fba19da1f9b8ce
Javad <J.Evazzadeh@gmail.com>
2015-05-30 10:49:30 +0430

    update trans and add arabic language

---
+++ includes/languages/trans_twig.php
@@ -0,0 +1,74 @@
+<?php
+function transtext()
+{
+
+	//-------------------------------------------------------content\home\display.html
+	echo T_("logout form all service");                                               // Line 19
+	echo T_("login to all service");                                                  // Line 21
+	echo T_("One account for all service");                                           // Line 35
+	echo T_("now only");                                                              // Line 37
+	echo T_("secure online storage");                                                 // Line 37
+	echo T_("Talambar");                                                              // Line 37
+	echo T_("is ready to use at alfa version.");                                      // Line 37
+	echo T_("our new service is comming soon.");                                      // Line 38 Seperate
+	echo T_("Ermile is our brand");                                                   // Line 37
+	echo T_("Experience our service in your language");                               // Line 42
+
+	//------------------------------------------------content_account\home\layout.html
+
+	//----------------------------------------------content_account\login\display.html
+	echo T_("Click here for recovery your password");                                 // Line 6
+	echo T_("Can't access your account?");                                            // Line 6
+	echo T_("Don't have an account?");                                                // Line 7
+	echo T_("Click here to create an account");                                       // Line 7
+	echo T_("Sign Up");                                                               // Line 7
+
+	//-------------------------------------------content_account\recovery\display.html
+	echo T_("click here to login to your account");                                   // Line 6
+	echo T_("are you remember your password!?");                                      // Line 6
+
+	//---------------------------------------------content_account\signup\display.html
+	echo T_("terms of service");                                                      // Line 6
+	echo T_("by signing up, you agree to the");                                       // Line 6 Seperate
+	echo T_("already have an account?");                                              // Line 7
+	echo T_("click here to sign in to your account");                                 // Line 7
+	echo T_("Sign In");                                                               // Line 7
+
+	//------------------------------------------------content_account\sms\display.html
+	echo T_("receive message callback!");                                             // Line 7
+
+	//---------------------------------------content_account\verification\display.html
+	echo T_("we send a verification code for you");                                   // Line 6
+	echo T_("please check your mobile and enter the code");                           // Line 7
+	echo T_("don't receive message?");                                                // Line 8
+
+	//------------------------------------content_account\verificationsms\display.html
+	echo T_("Please send number <b>2015</b> to below number");                        // Line 6 Seperate
+	echo T_("Then wait we receive your message and verificate your account");         // Line 8
+
+	//------------------------------------public_html\monitor\docs\html\copyright.html
+
+	//--------------------------------------public_html\monitor\docs\html\credits.html
+
+	//-----------------------------------public_html\monitor\docs\html\developers.html
+
+	//------------------------------------------public_html\monitor\docs\html\faq.html
+
+	//-------------------------------------public_html\monitor\docs\html\genindex.html
+
+	//----------------------------------------public_html\monitor\docs\html\index.html
+
+	//--------------------------------------public_html\monitor\docs\html\install.html
+
+	//----------------------------------------public_html\monitor\docs\html\intro.html
+
+	//-------------------------------------public_html\monitor\docs\html\modindex.html
+
+	//---------------------------------public_html\monitor\docs\html\requirements.html
+
+	//---------------------------------------public_html\monitor\docs\html\search.html
+
+	//----------------------public_html\static\js\datatable\datatable-langs\index.html
+
+}
+?>
