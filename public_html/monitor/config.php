2f012296c2068f19ee19cc665f24785280945bc5
Javad <J.Evazzadeh@live.com>
2016-04-24 09:01:15 +0430

    update monitor config

--- public_html/monitor/config.php
+++ public_html/monitor/config.php
@@ -2,5 +2,5 @@
 define('PSM_DB_HOST', 'localhost');
 define('PSM_DB_NAME', 'monitor');
 define('PSM_DB_USER', 'ermile');
-define('PSM_DB_PASS', 'ermile@#$567');
+define('PSM_DB_PASS', 'Ermile@#$1233');
 define('PSM_DB_PREFIX', 'psm_');
