5b9f4267ee26ae9d0b9bd445c9ffd8b79013040b
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-05-09 19:26:23 +0430

    some fix on dash

--- code/dash/dash-2.0.0.js
+++ code/dash/dash-2.0.0.js
@@ -1,7 +1,8 @@
-  $('#sidebar div > span').click(function()
+  $('#sidebar ul.sidenav > li > a').click(function()
   {
     var menuTitle = $(this);
     var submenu   = $(this).parent().children('ul');
+    console.log(submenu);
     if (submenu)
     {
       submenu.stop().slideToggle(300);
