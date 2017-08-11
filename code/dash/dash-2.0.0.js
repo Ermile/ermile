ec57bfb963a26a24dd42aca0f445d5950d8d7ba1
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-08-11 11:43:23 +0430

    fix language design and change address of language on each page on enter and dash

--- code/dash/dash-2.0.0.js
+++ code/dash/dash-2.0.0.js
@@ -9,9 +9,62 @@ route('*', function ()
   runDataResponse();
   showFilePreview();
   responsiveSidebar();
+  setLanguageURL();
 
 });
 
+
+
+/**
+ * set link of language on each page
+ */
+function setLanguageURL()
+{
+  var urlPath     = window.location.pathname;
+  var urlHash     = window.location.hash;
+  var indexOfLang = urlPath.indexOf('/' + $('html').attr('lang') + '/');
+  var urlBase     = $('base').attr('href');
+  urlBase         = urlBase.substr(0, urlBase.indexOf('/', 9));
+
+  if(indexOfLang === 0)
+  {
+    urlPath = urlPath.substr(4);
+  }
+  else
+  {
+    urlPath = urlPath.substr(1);
+  }
+
+  $('.langlist a').each(function(key, index)
+  {
+    var lang = $(index).attr('hreflang');
+    // if we are in this language
+    if(lang == $('html').attr('lang'))
+    {
+      $(index).attr('class', 'isActive');
+    }
+    if(lang == 'en')
+    {
+      lang = '';
+    }
+    else if(lang == $('html').attr('lang'))
+    {
+      // lang = '/' + lang;
+    }
+    var myUrl = urlPath;
+    if(lang)
+    {
+      myUrl = lang + '/' + myUrl;
+    }
+    // add hash if exist
+    if(urlHash)
+    {
+      myUrl += urlHash;
+    }
+    myUrl = urlBase + '/' + myUrl;
+    $(index).attr('href', myUrl.trim('/'));
+  })
+}
 
 
 /**
--- code/enter/enter.js
+++ code/enter/enter.js
@@ -16,6 +16,7 @@ route('*', function ()
 {
 	// add check handler to all data-require elements
 	runDataRequire(true);
+	setLanguageURL();
 
 });
 
@@ -42,7 +43,7 @@ function runDataRequire(_firstTime)
 		}
 
 
-		console.log(checkResult);
+		// console.log(checkResult);
 		// if its true and okay show it
 		if(checkResult)
 		{
@@ -68,7 +69,62 @@ function runDataRequire(_firstTime)
 		}
 	});
 }
+https://tejarak.dev/enter/
 
+/**
+ * set link of language on each page
+ */
+function setLanguageURL()
+{
+  var urlPath     = window.location.pathname;
+  var urlHash     = window.location.hash;
+  var indexOfLang = urlPath.indexOf('/' + $('html').attr('lang') + '/');
+  var urlBase     = $('base').attr('href');
+  urlBase         = urlBase.substr(0, urlBase.indexOf('/', 9));
+  console.log(urlPath);
+  console.log(indexOfLang);
+  console.log(urlBase);
+
+  if(indexOfLang === 0)
+  {
+    urlPath = urlPath.substr(4);
+  }
+  else
+  {
+    urlPath = urlPath.substr(1);
+  }
+  console.log(urlPath);
+
+  $('.langlist a').each(function(key, index)
+  {
+    var lang = $(index).attr('hreflang');
+    // if we are in this language
+    if(lang == $('html').attr('lang'))
+    {
+      $(index).attr('class', 'isActive');
+    }
+    if(lang == 'en')
+    {
+      lang = '';
+    }
+    else if(lang == $('html').attr('lang'))
+    {
+      // lang = '/' + lang;
+    }
+    var myUrl = urlPath;
+    if(lang)
+    {
+      myUrl = lang + '/' + myUrl;
+    }
+    // add hash if exist
+    if(urlHash)
+    {
+      myUrl += urlHash;
+    }
+    myUrl = urlBase + '/' + myUrl;
+    $(index).attr('href', myUrl.trim('/'));
+  })
+}
 
 
 function allowTogglePass()
