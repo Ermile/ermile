8a0acb3fa84b0b587ff61cd8f222d62e16ee08d7
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-08-11 11:55:35 +0430

    bugfix on some conditions

--- code/dash/dash-2.0.0.js
+++ code/dash/dash-2.0.0.js
@@ -22,7 +22,7 @@ function setLanguageURL()
 {
   var urlPath     = window.location.pathname;
   var urlHash     = window.location.hash;
-  var indexOfLang = urlPath.indexOf('/' + $('html').attr('lang') + '/');
+  var indexOfLang = urlPath.indexOf('/' + $('html').attr('lang'));
   var urlBase     = $('base').attr('href');
   urlBase         = urlBase.substr(0, urlBase.indexOf('/', 9));
 
