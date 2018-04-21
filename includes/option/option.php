26ae3160d683d033f7692f164baf3be1ba8a236a
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-04-21 20:19:07 +0430

    fix option

--- includes/option/option.php
+++ includes/option/option.php
@@ -16,6 +16,13 @@ self::$language =
 self::$url['tld']                   = 'com';
 self::$url['protocol']              = 'https';
 
+
+/**
+ * call kavenegar template
+ */
+self::$config['enter']['call']                = true;
+self::$config['enter']['call_template_fa'] = 'ermile-fa';
+self::$config['enter']['call_template_en'] = 'ermile-en';
 self::$config['enter']['verify_telegram'] = false;
 self::$config['enter']['verify_sms']      = true;
 self::$config['enter']['verify_call']     = true;
