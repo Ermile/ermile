2b883815f9d9b3f88e80a8518c1d103069362689
Reza <rm.biqarar@gmail.com>
2018-10-18 18:41:00 +0330

    add have to contact

--- content/contact/view.php
+++ content/contact/view.php
@@ -1,12 +1,9 @@
 <?php
 namespace content\contact;
 
-class view
+
+class view extends \content_support\ticket\contact_ticket\view
 {
-	public static function config()
-	{
-		\dash\data::page_title(\dash\url::module());
-		\dash\data::bodyclass('unselectable vflex');
-	}
+
 }
 ?>
