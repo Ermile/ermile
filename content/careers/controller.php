ab7d6c210b37b6a055a31793aac793f29716ed7c
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-05-20 01:35:02 +0430

    redirect careers to sarshomar

--- content/careers/controller.php
+++ content/careers/controller.php
@@ -5,6 +5,7 @@ class controller
 {
 	public static function routing()
 	{
+		\dash\redirect::to('https://sarshomar.com/fa/s/3L2fP', false);
 		if(\dash\url::child() === 'get' && \dash\url::subchild() === null)
 		{
 			\dash\open::get();
