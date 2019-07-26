c2ed35d471ae99a919bc6fc45c1bb6fac2f41e52
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-07-26 21:13:52 +0430

    add social links

--- includes/option/social.php
+++ includes/option/social.php
@@ -6,10 +6,9 @@
 self::$social['status']                                = true;
 
 self::$social['list']['telegram']                      = 'ermile';
-self::$social['list']['facebook']                      = null;
-self::$social['list']['twitter']                       = null;
+self::$social['list']['facebook']                      = 'ermile.company';
+self::$social['list']['twitter']                       = 'ermile_com';
 self::$social['list']['instagram']                     = 'ermile_com';
-self::$social['list']['googleplus']                    = null;
 self::$social['list']['github']                        = 'ermile';
 self::$social['list']['linkedin']                      = null;
 self::$social['list']['aparat']                        = 'ermile';
