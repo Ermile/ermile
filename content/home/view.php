19bd528674e862a09950508c07c9b5ec3e7048dc
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-07-28 14:48:52 +0430

    add cover for homepage

--- content/home/view.php
+++ content/home/view.php
@@ -10,6 +10,7 @@ class view
 		\dash\data::page_title(T_('As easy as ABC'));
 		\dash\data::page_desc(T_('We provide a wide range of enterprise services to you.'));
 		\dash\data::page_special(true);
+		\dash\data::page_cover(\dash\url::static(). '/images/bg/bg-ermile-cover.jpg');
 
 		if(\dash\url::module() === null)
 		{
