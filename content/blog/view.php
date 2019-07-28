4cffca9affb36dcd794a5e91bb1a1408fecd2d93
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-07-28 14:33:59 +0430

    improve seo

--- content/blog/view.php
+++ content/blog/view.php
@@ -9,6 +9,7 @@ class view
 		\dash\data::allPostList(\dash\app\posts::get_post_list(['pagenation' => true]));
 
 		\dash\data::page_desc(T_('Know about our stories and events'));
+		\dash\data::page_cover(\dash\url::static(). '/images/homepage/bg-ermile-blog.jpg');
 	}
 }
 ?>
